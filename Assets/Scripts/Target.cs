564202366211042082370b6fdb2f170f1dd41653
SakRey63 <152211412+SakRey63@users.noreply.github.com>
2025-01-27 19:03:53 +0300

    I rewrote it

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,6 +6,7 @@ public class Target : MonoBehaviour
     [SerializeField] private float _speed;
     
     private int _currentPosition = 0;
+    private float _epsilon = 0.1f;
     private Vector3 _currentTargetPosition;
     
     private void Start()
@@ -17,7 +18,7 @@ public class Target : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, _currentTargetPosition, _speed * Time.deltaTime);
 
-        if (transform.position == _currentTargetPosition)
+        if ((transform.position - _currentTargetPosition).sqrMagnitude < _epsilon)
         {
             _currentPosition = ++_currentPosition % _movePositions.Length;
             
